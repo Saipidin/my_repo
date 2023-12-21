ee63bbf4118a9c1565e368a9da4881877137b6ba
Hasanov Saipidin <hasanovsajpidin@gmail.com>
2023-12-21 22:32:11 +0600

    Project

---
+++ Задание/1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение M:");
+        double m = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        double n = double.Parse(Console.ReadLine());
+
+        NaturalNumbers(m, n);
+
+        Console.ReadLine();
+    }
+
+    static void NaturalNumbers(double m, double n)
+    {
+        if (m <= n)
+        {
+            if (m % 1 == 0 && m > 0)
+            {
+                Console.WriteLine((int)m);
+            }
+
+            NaturalNumbers(m + 1, n);
+        }
+    }
+}
---
+++ Задание/3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        int[] myArray = RandomNUM(10, random);
+
+        Console.WriteLine("Элементы обычного массива:");
+        PrintArray(myArray);
+
+        Console.WriteLine("\nЭлементы массива, начиная с конца:");
+        ArrayReversed(myArray, myArray.Length - 1);
+
+        Console.ReadLine();
+    }
+
+    static int[] RandomNUM(int length, Random random)
+    {
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = random.Next(1, 100); // Генерация случайных чисел от 1 до 100
+        }
+
+        return array;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void ArrayReversed(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(array[index]);
+            ArrayReversed(array, index - 1);
+        }
+    }
+}
